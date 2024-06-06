bd1481731200ac6328f39a6bbd533fb2ab90e0be
Olga Goiko <olga.a10@ukr.net>
2024-06-06 22:31:55 +0300

    add task-4 files

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,15 @@
+const registerForm = document.querySelector('.login-form');
+registerForm.addEventListener('submit', handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+  }
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
+}
